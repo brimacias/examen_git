6edb6d98a1bf550b407bfbe8ed0beb9b8d4f3aea
brimacias <95747897+brimacias@users.noreply.github.com>
2022-12-05 17:51:10 +0100

    corregido y mejorado

---
+++ src/Clase.java
@@ -0,0 +1,14 @@
+public class Clase {
+    int entero;
+    String string;
+
+    public Clase(int i, String string) {
+        this.entero = i;
+        this.string = string;
+    }
+
+    @Override
+    public String toString() {
+        return "Objeto { i=" + entero + ", a='" + string + "\'}";
+    }
+}
